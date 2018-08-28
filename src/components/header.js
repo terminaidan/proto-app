b34f7b1771698fc69a5130ab48c0e6e6be189f52
Aidan Walker <aidanwalker@aidans-mbp.lan>
2018-08-28 20:04:08 +0100

    Chapter 3 Adaptive layouts

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,19 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import logo from '../images/logo-designcode.svg'
+import './Header.css'
+
+// or you can rquire the logo directly to the link like 
+// <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30"/></Link>
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className="Header">
+    <div className="HeaderGroup">
+      <Link to="/"><img src={logo} width="30"/></Link>
+      <Link to="/courses">Courses</Link>
+      <Link to="/downloads">Downloads</Link>
+      <Link to="/worskshops">Workshops</Link>
+      <Link to="/buy"><button>Buy</button></Link>
     </div>
   </div>
 )
